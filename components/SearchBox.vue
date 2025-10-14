dc644549cdb79ccbb3ed7a49f6db497a6fdadea9
amirhosein sajjadi <canadavancouver2003@gmail.com>
2025-10-14 22:32:05 +0330

    create indext banner

---
+++ components/SearchBox.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bg-blue-500 w-[60%] z-10">
+        adsnlasnd
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import PersianDateTimePicker from 'vue3-persian-datetime-picker'
+import CityPicker from './CityInput.vue'
+
+const origin = ref('')
+const destination = ref('')
+const flightDates = ref({ start: null, end: null })
+
+const searchFlights = () => {
+  if (origin.value && destination.value && flightDates.value.start && flightDates.value.end) {
+    console.log('جستجو:', {
+      origin: origin.value,
+      destination: destination.value,
+      dates: flightDates.value
+    })
+    // اینجا API کال برای گرفتن پروازها
+  } else {
+    alert('لطفاً تمام فیلدها را پر کنید')
+  }
+}
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col">
+        <div class=" w-full h-[28rem] flex flex-col justify-center items-center relative p-10 overflow-hidden">
+            <div class="rounded-[30px] h-full flex overflow-hidden object-cover relative">
+                <div class="w-full flex flex-col justify-start items-start absolute top-1/2 -translate-y-1/2 right-10">
+                    <h1 class="text-white font-extrabold text-[1.3rem] tracking-tighter">
+                        مارکوپرو ، مرکز رزرواسیون خدمات گردشگری ؛ هتل ، پرواز و تور
+                    </h1>
+                    <h2 class="text-white text-[1rem] text-justify font-normal tracking-tight">
+                        بهترین قیمت ها با تنوع در ارائه خدمات گردشگری
+                    </h2>
+                </div>
+                <img src="../public/imgs/home-banner.jpg" alt="مارکوپرو"
+                    class="object-cover rounded-[1.2rem] w-full h-full"            
+                />
+            </div>
+        </div>
+        <SearchBox />
+    </div>
+
+</template>
+<script setup>
+//Components
+import SearchBox from '~/components/SearchBox.vue';
+
+</script>
+<style scoped>
+</style>
